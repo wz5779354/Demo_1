81f1f81b6d1529b78cb14a67eec578f4099e6c6a
wangze_myself <m15650701826@163.com>
2017-11-03 15:27:56 +0800

    add MainActivity a Button

--- app/src/main/java/com/example/wangze/demo_1/MainActivity.java
+++ app/src/main/java/com/example/wangze/demo_1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wangze.demo_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void enterToSecondAct(View view){
+    startActivity(new Intent(this,SecondActivity.class));
     }
 }
