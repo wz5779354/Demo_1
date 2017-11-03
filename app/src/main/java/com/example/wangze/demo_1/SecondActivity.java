ba59b1b432a06dfaeb8e0e6f0c4b235f56643dc3
wangze_myself <m15650701826@163.com>
2017-11-03 16:27:19 +0800

    add ThirdActivity

--- app/src/main/java/com/example/wangze/demo_1/SecondActivity.java
+++ app/src/main/java/com/example/wangze/demo_1/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.wangze.demo_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+    public void enterToThirdActivity(View view){
+        startActivity(new Intent(this,ThirdActivity.class));
     }
 }
